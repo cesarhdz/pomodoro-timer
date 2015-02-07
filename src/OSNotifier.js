91beabaa739c6b2cc6e79d16a4ca6293c8e1808b
cesarhdz <mrshortcuts@gmail.com>
2015-02-07 08:58:25 -0600

    OSNotifier.notify added to warp notde-notifier

--- src/OSNotifier.js
+++ src/OSNotifier.js
@@ -2,9 +2,7 @@ var
 notifier = require('node-notifier'),
 path     = require('path'),
 
-
 icon = path.join(__dirname, '..', 'assets', 'pomodoro-timer.png')
-
 
 
 function toMinutes(time){
@@ -24,60 +22,75 @@ function toMinutes(time){
 }
 
 
-function OSNotifier(){}
+function OSNotifier(){
+	this.useSounds = true
+	this.reminderTime = 60 //seconds
+}
+
+
+
+/**
+ * Wrapper to generate notifications using notifier.noty
+ * Adds paths to assets and adds a sound to
+ * the notification when useSound is true
+ *
+ * @param  {notification} notification
+ * @arg    {String} title   
+ * @arg    {String} message 
+ * @arg    {String} sound   
+ */
+OSNotifier.prototype.notify = function(notification){
+	notification.icon = icon
+	notification.sound = (this.useSounds && notification.sound) 
+
+	return notifier.notify(notification)
+}
 
 
 OSNotifier.prototype.connect = function(app){
 
+	var 
+	taskReminder,
+	osNotifier = this
+
 	app.on('task.timeover.notification', function(timeLeft, task){
-	  notifier.notify({ 
+	  osNotifier.notify({
 	  	title: 'Tic Tac!',
 	    message: 'You have ' +  toMinutes(timeLeft) + ' minutes to complete your task!', 
-	    icon: icon 
+	    sound: true
 	  })
 	})
-
 
 	app.on('shortBreak.start', function(time){
-	  // Growl notification
-	  notifier.notify({ 
+	  osNotifier.notify({ 
 	  	title: 'Break time',
 	    message: 'It\'s time to have a Break!', 
-	    icon: icon
 	  })
 	})
 
 
 	app.on('shortBreak.timeover', function(){
-	  // Growl notification
-	  notifier.notify({
+	  osNotifier.notify({
 	  	title:'Break Time is over',
 	    message: 'You have to get back to work!', 
-	    icon: icon
+	    sound: true
 	  })
-	})
 
-
-	var taskReminder
-
-	app.on('shortBreak.timeover', function(){
 	  // Notify when break time is over, but you haven't choose a task
 	  taskReminder = setInterval(function(){
-	    notifier.notify({
+	    osNotifier.notify({
 	      title:'Are you still there?',
 	      message: "You haven't choose any task to work with",
-	      icon:icon
+	      sound: true
 	    })
-	  }, 60 * 1000)
+	  }, osNotifier.reminderTime * 1000)
 	})
 
 
 	app.on('task.start', function(){
 	  if(taskReminder) clearInterval(taskReminder)
 	})
-
 }
-
 
 
 module.exports = OSNotifier
